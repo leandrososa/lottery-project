0d40f2b4a90bc8ed25d4b3ba60ddfb5c868dc5e7
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-02-23 23:59:06 -0300

    node testing / compiling / deploying env init

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'lero lero',
+    'https://rinkeby.infura.io/v3/1871c33c3a6942709fc9f3b0faf9f717'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    //console.log(accounts[0]);
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to ', result.options.address);
+};
+
+deploy();
