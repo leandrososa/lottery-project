9adf4391c571ee8ecafa9bb14e8ab98e3664e5d6
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-02-24 00:30:28 -0300

    added deploy & player enter tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,37 +1,36 @@
-/* const assert = require('assert');
-const ganache = require('ganache-cli');
+const assert = require('assert');
+const ganache = require('ganache-cli'); // Contract testing network
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require ('../compile');
 
+let lottery;
 let accounts;
-let inbox;
 
 beforeEach( async() =>{
-    //Get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
-    // Use one of those accounts to deploy
-    // the contract
-
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-            .deploy({ data: bytecode, arguments: ['Hi There!'] })
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({ data: bytecode })
             .send({ from: accounts[0], gas: 1000000 });
 });
 
-describe('Inbox', () => {
+describe ('Lottery contract', () => {
     it('deploys a contract', () => {
-        assert.ok(inbox.options.address);
-    });
-
-    it('has a default message', async() => {
-        const message = await inbox.methods.message().call();
-        assert.strictEqual(message, 'Hi There!')
-    });
-
-    it('can change message', async() =>{
-        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
-        const message = await inbox.methods.message().call();
-        assert.strictEqual(message, 'bye');
+        assert.ok(lottery.options.address);
     })
-}) */
+
+    it('allows one account to enter', async () =>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.strictEqual(accounts[0], players[0]);
+        assert.strictEqual(1, players.length);
+    })
+})
